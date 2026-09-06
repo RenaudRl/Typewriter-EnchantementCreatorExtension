370dd33d288515cd2b8dc48a3908daa3e43c0222
renaud <rlpirou@gmail.com>
2026-09-06 07:52:09 +0200

    chore(api): target Typewriter 0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 ﻿plugins {
-    kotlin("jvm") version "2.3.20"
-    id("com.typewritermc.module-plugin") version "2.1.0"
+    kotlin("jvm") version "2.2.10"
+    id("com.typewritermc.module-plugin") version "2.2.0"
 }
 
 group = "btcrenaud"
-version = "0.8"
+version = "0.9"
 
 repositories {
 }
@@ -25,7 +25,7 @@ typewriter {
         description = "EnchantmentCreator extension for TypeWriter providing advanced " +
             "enchantment creation and management tools for Paper 1.21+. Fully compatible " +
             "with the official TypeWriter engine."
-        engineVersion = "0.9.0-beta-175"
+        engineVersion = "0.9.0-beta-176"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
         dependencies {
             dependency("typewritermc", "Basic")
